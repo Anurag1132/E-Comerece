83d0df27406935e7679eccb4eeb243cf5c0d1e8c
2092062 <akhilreddychilukuri@gmail.com>
2021-10-22 19:49:02 -0400

    user list added

---
+++ app/src/main/java/com/example/storetodoor/userAdapter.java
@@ -0,0 +1,74 @@
+package com.example.storetodoor;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.core.View;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+public class userAdapter extends RecyclerView.Adapter<userAdapter.myviewholder>  {
+
+
+    ArrayList<Pojo> userlist;
+    FirebaseUser user;
+
+    public userAdapter(ArrayList<Pojo> userlist) {
+        this.userlist = userlist;
+    }
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_list_recycleview, parent, false);
+        return new myviewholder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull myviewholder holder, int position) {
+
+        holder.userlist_firstname.setText(userlist.get(position).getFirstName());
+        holder.userlist_lastname.setText(userlist.get(position).getLastName());
+        holder.userlist_city.setText(userlist.get(position).getCity());
+        holder.userlist_mobile.setText(userlist.get(position).getMobilenumber());
+        holder.userlist_email.setText(userlist.get(position).getEmail());
+
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        return userlist.size();
+
+    }
+
+
+
+    class myviewholder extends RecyclerView.ViewHolder  {
+        TextView userlist_firstname,userlist_lastname,userlist_city, userlist_mobile,userlist_email;
+
+
+
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+            userlist_firstname = itemView.findViewById(R.id.user_list_firstname);
+            userlist_lastname=itemView.findViewById(R.id.user_list_lastname);
+            userlist_city = itemView.findViewById(R.id.user_list_city);
+            userlist_mobile = itemView.findViewById(R.id.user_list_mobile);
+             userlist_email=itemView.findViewById(R.id.user_list_email);
+
+        }
+
+
+    }
+}
+
+
+
